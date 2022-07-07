57ff764af46fa1d5c7afe32c688dbaa9ffb30c51
gontarev <gontarevsanya@gmail.com>
2022-07-07 21:48:45 +0300

    manage commands

---
+++ catalog/management/commands/delete_by_id.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Delete users by id"
+
+    def add_arguments(self, parser):
+        parser.add_argument("user_id", nargs="+", type=int, help="enter user id to delete ")
+
+    def handle(self, *args, **options):
+
+        if User.objects.get(is_superuser=True).id in options["user_id"]:
+            superuser_id = User.objects.get(is_superuser=1).id
+            raise CommandError(f"superuser with id({superuser_id}) in the list, deletion is not possible")
+        delete_user = User.objects.filter(pk__in=options["user_id"]).delete()
+        self.stdout.write("Delete %s" % delete_user)
+
+
