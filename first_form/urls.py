644e8bb9c809f1c634bb8ba17d4141e8a60f4ce9
gontarev <gontarevsanya@gmail.com>
2022-07-15 12:27:50 +0300

    create and update

--- first_form/urls.py
+++ first_form/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from first_form.views import index
+from first_form.views import index, model_form, model_update_form
 
 app_name = "firstform"
 
-urlpatterns = [path('', index, name='index'), ]
+urlpatterns = [
+    path('triangle/', index, name='index'),
+    path('person/', model_form, name='person-form'),
+    path('person/<int:pk>/', model_update_form, name='person-update-form'),
+]
--- firstsite/urls.py
+++ firstsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('triangle/', include('first_form.urls')),
+    path('', include('first_form.urls')),
 ]
