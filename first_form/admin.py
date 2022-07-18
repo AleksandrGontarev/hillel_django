97b7c651a269e33b7e443ace37646cf9655a6f8c
gontarev <gontarevsanya@gmail.com>
2022-07-18 12:52:15 +0300

    fix middleware add filter in admin

--- first_form/admin.py
+++ first_form/admin.py
@@ -6,3 +6,5 @@ from first_form.models import Log
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     list_display = ('path', 'method', 'timestamp')
+    date_hierarchy = 'timestamp'
+    list_filter = ['method', date_hierarchy]
--- first_form/middleware.py
+++ first_form/middleware.py
@@ -3,12 +3,12 @@ from django.utils.deprecation import MiddlewareMixin
 from first_form.models import Log
 
 
-class LogMiddleware(MiddlewareMixin):
+class LogMiddleware:
 
-    def _init_(self, get_response):
+    def __init__(self, get_response):
         self.get_response = get_response
 
-    def _call_(self, request):
+    def __call__(self, request):
         response = self.get_response(request)
         return response
 
